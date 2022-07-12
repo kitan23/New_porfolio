277f135b784e08d19ce6ca0982aa6ade00220060
kitan23 <ktran@hamilton.edu>
2022-07-12 10:49:04 +0700

    fix: dependency tree conflict

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,28 @@
-import { ChakraProvider } from "@chakra-ui/provider";
-import Layout from "../components/layouts/main"; 
-import theme from "../libs/theme";
-import Fonts from "../components/fonts";
-import { AnimatePresence } from "framer-motion";
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '../components/layouts/main'
+import theme from '../libs/theme'
+import Fonts from '../components/fonts'
+import { AnimatePresence } from 'framer-motion'
 
 const Website = ({ Component, pageProps, router }) => {
-   return (
-      <ChakraProvider theme={theme}>
-         <Fonts />
-         <Layout router = {router}>
-            <AnimatePresence exitBeforeEnter initial={true} onExitComplete={()=>{
-                if(typeof window !== 'undefined'){
-                    window.scrollTo({top:0})
-                }
-            }}>
-            <Component {...pageProps} key = {router.route} />
-            </AnimatePresence>
-         </Layout>
-      </ChakraProvider>
-   )
+  return (
+    <ChakraProvider theme={theme}>
+      <Fonts />
+      <Layout router={router}>
+        <AnimatePresence
+          exitBeforeEnter
+          initial={true}
+          onExitComplete={() => {
+            if (typeof window !== 'undefined') {
+              window.scrollTo({ top: 0 })
+            }
+          }}
+        >
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </Layout>
+    </ChakraProvider>
+  )
 }
 
-export default Website; 
+export default Website
