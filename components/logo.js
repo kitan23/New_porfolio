29c814258d024a2b3b5800df39757e26b0a32f20
kitan23 <ktran@hamilton.edu>
2023-05-30 14:27:46 -0400

    change preferred name info

--- components/logo.js
+++ components/logo.js
@@ -1,38 +1,40 @@
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link'
+import Image from 'next/image'
 
-import { Text, useColorModeValue } from "@chakra-ui/react"; 
-import styled from "@emotion/styled";
+import { Text, useColorModeValue } from '@chakra-ui/react'
+import styled from '@emotion/styled'
 
-const LogoBox = styled.span `
-font-weight: bold; 
-font-size: 18px; 
-display: inline-flex; 
-align-items: center; 
-height: 30px;
-line-height: 20px; 
-padding: 10px; 
+const LogoBox = styled.span`
+  font-weight: bold;
+  font-size: 18px;
+  display: inline-flex;
+  align-items: center;
+  height: 30px;
+  line-height: 20px;
+  padding: 10px;
 
-&:hover img {
-   transform: rotate(20deg); 
-}
+  &:hover img {
+    transform: rotate(20deg);
+  }
 `
 const Logo = () => {
-   const footPrintImg = `/images/footprint${useColorModeValue('','-dark')}.png`
-   return (
-      <Link href="/" scroll={false}>
-         <a>
-            <LogoBox>
-               <Image src={footPrintImg} width={20} height={20} alt="logo" />
-               <Text color = {useColorModeValue('gray.800', 'whiteAlpha.900')}
-               fontFamily = 'M PLUS Rounded 1c'
-               fontWeight = "bold"
-               ml={3}>
-                  Kien Tran
-               </Text>
-            </LogoBox>
-         </a>
-      </Link>
-   )
+  const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
+  return (
+    <Link href="/" scroll={false}>
+      <a>
+        <LogoBox>
+          <Image src={footPrintImg} width={20} height={20} alt="logo" />
+          <Text
+            color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+            fontFamily="M PLUS Rounded 1c"
+            fontWeight="bold"
+            ml={3}
+          >
+            Kyle Tran
+          </Text>
+        </LogoBox>
+      </a>
+    </Link>
+  )
 }
-export default Logo; 
+export default Logo
