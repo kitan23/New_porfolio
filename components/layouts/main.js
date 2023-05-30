b0518d48861ebe12a6f5813eeeeb7c688cf2f4f3
kitan23 <ktran@hamilton.edu>
2023-05-30 14:28:09 -0400

    change preferred name info

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,27 +1,27 @@
 import Head from 'next/head'
 import Navbar from '../navbar'
-import {Box, Container} from "@chakra-ui/react"
+import { Box, Container } from '@chakra-ui/react'
 import VoxelDog from '../voxel-dog'
-import NoSsr  from '../no-ssr'
+import NoSsr from '../no-ssr'
 
-const Main = ({children, router}) => {
+const Main = ({ children, router }) => {
   return (
     <Box as="main" pb={8}>
       <Head>
-        <meta name = "viewport" content = "width=device-width, initial-scale=1"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-          <link
-              rel="stylesheet"
-              href="https://video-react.github.io/assets/video-react.css"
-          />
-        <title> Kien Tran - Homepage </title> 
-      </Head> 
+        <link
+          rel="stylesheet"
+          href="https://video-react.github.io/assets/video-react.css"
+        />
+        <title> Kyle Tran - Homepage </title>
+      </Head>
 
-      <Navbar path={router.asPath}/>
+      <Navbar path={router.asPath} />
       <Container maxW="container.md" pt={14}>
         <NoSsr>
-          <VoxelDog/>
+          <VoxelDog />
         </NoSsr>
         {children}
       </Container>
